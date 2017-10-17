8df64b61b4b00eba4e46fc87f8c7aa1cdb72cf24
cakrafals <cakrafals@gmail.com>
2017-10-17 12:13:06 +0700

    tambahan

---
+++ app/src/main/java/com/example/khalifa/kotlin2/continuebreak2.kt
@@ -0,0 +1,12 @@
+package com.example.khalifa.kotlin2
+
+/**
+ * Created by khalifa on 17/10/17.
+ */
+fun main(args: Array<String>){
+    for(count in 1..5){
+        if(count==4)
+            break
+        println("count: $count")
+    }
+}
---
+++ app/src/main/java/com/example/khalifa/kotlin2/string.kt
@@ -0,0 +1,21 @@
+package com.example.khalifa.kotlin2
+
+/**
+ * Created by khalifa on 17/10/17.
+ */
+fun main(args: Array<String>){
+    var title="Selamat Datang"
+    println(title)
+    println("title:"+ title)
+    println("title:$title")
+
+    var name="cakra" + "aji"
+    println("nama:$name")
+    println("Second char"+title[1])
+    println("title"+ title.toLowerCase())
+    println("title"+ title.toUpperCase())
+    println("title"+ title.split(""))
+    println("title"+ title.trim())
+
+
+}
