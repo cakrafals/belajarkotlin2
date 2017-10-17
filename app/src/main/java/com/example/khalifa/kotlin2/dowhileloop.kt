3064c54b33aea48eab33f4014513b896cc2e4073
cakrafals <cakrafals@gmail.com>
2017-10-17 11:52:41 +0700

    kotlin2

---
+++ app/src/main/java/com/example/khalifa/kotlin2/dowhileloop.kt
@@ -0,0 +1,13 @@
+package com.example.khalifa.kotlin2
+
+/**
+ * Created by khalifa on 17/10/17.
+ */
+fun main(args: Array<String>){
+    var i=6
+    do {
+        println("hello")
+        i++;
+    }
+    while(i<=5)
+}
---
+++ app/src/main/java/com/example/khalifa/kotlin2/whileloop.kt
@@ -0,0 +1,11 @@
+package com.example.khalifa.kotlin2
+
+/**
+ * Created by khalifa on 17/10/17.
+ */
+fun main(args: Array<String>){
+    var i=1
+    while(i<=5)
+        println("hello")
+    i++
+}
